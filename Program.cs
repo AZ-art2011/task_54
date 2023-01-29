38ed361f1d672771a5bc94de9a544b8cfa9c5cb9
Alexander <mypost@example.com>
2023-01-29 10:40:54 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 54: Задайте двумерный массив. Сделайте упорядочивание по убыванию 
+// элементы каждой строки двумерного массива.
+
+Console.Clear();
+
+int[,] Matrix = new int[3, 4];
+RecordTempMatrix(Matrix);
+Console.WriteLine($"Исходный массив");
+WriteTempMatrix(Matrix);
+SortMatrix(Matrix);
+Console.WriteLine();
+Console.WriteLine($"Отсортированный массив");
+WriteTempMatrix(Matrix);
+
+void RecordTempMatrix(int[,] TempMatrix)
+{
+    for (int i = 0; i < TempMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < TempMatrix.GetLength(1); j++)
+        {
+            TempMatrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void SortMatrix(int[,] TempMatrix)
+{
+    for (int i = 0; i < TempMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < TempMatrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < TempMatrix.GetLength(1) - 1; k++)
+            {
+                if (TempMatrix[i, k] < TempMatrix[i, k + 1])
+                {
+                    int temp = TempMatrix[i, k + 1];
+                    TempMatrix[i, k + 1] = TempMatrix[i, k];
+                    TempMatrix[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+void WriteTempMatrix(int[,] TempMatrix)
+{
+    for (int i = 0; i < TempMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < TempMatrix.GetLength(1); j++)
+        {
+            Console.Write($"{TempMatrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
